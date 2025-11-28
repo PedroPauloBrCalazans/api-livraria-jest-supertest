2c0d758af119d7c875791c98eaa16e19aac7093e
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-11-28 00:05:06 -0300

    feat: Add comentários

--- src/test/services/authService.test.js
+++ src/test/services/authService.test.js
@@ -5,17 +5,20 @@ const authService = new AuthService();
 
 describe("Testando a authService de cadastrar", () => {
   it("O usuário deve possuir um nome, email e senha", async () => {
+    //arrange = quais informações queremos validar
     const userMock = {
       nome: "Pedro Paulo",
       email: "pedro@Mail.com.br",
     };
 
+    //act = vamos buscar as informações na função, service
     const usuarioSalvo = authService.cadastrarUsuario(userMock);
 
+    //assert = vamos comparar o arrange com nosso assert
     await expect(usuarioSalvo).rejects.toThrowError(
       "A senha de usuário é obrigatório!"
     );
   });
 });
 
-// rejects valida erros
+// rejects valida erros ***** teste acima e conhecido como PADRÃO TRIPLE A
